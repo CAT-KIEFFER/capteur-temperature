d58951c7bc21aff2ec71b9a25817fc37c6f850ff
KIEFFER <loic.kieffer@hotmail.com>
2026-04-09 11:39:50 +0200

    V2: écriture dans fichier + préparation analyse

--- capteur_temp101.c
+++ capteur_temp101.c
@@ -1,27 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(void) {
 
-     char *status = "NOMINAL";
+     FILE *fichier = fopen("data.txt", "w");
+
+     if (fichier == NULL) {
+         printf("erreur ouverture fichier\n");
+         return 1;
+     }
+
+     srand(time(NULL));
 
      for (int cycle = 1; cycle <= 10; cycle ++) {
 
          int temp = rand() % 101;
+         char *status = "NOMINAL";
 
          if (temp < 15) {
              status = "LOW TEMP";
          }
-         else if (temp <= 80) {
-             status = "NOMINAL";
-         }
-         else {
+         else if (temp > 80) {
              status = "WARNING";
          }
 
-         printf("Cycle %d | Temp: %d | Status: %s\n", cycle, temp, status);
+         fprintf(fichier, "Cycle %d | Temp: %d | Status: %s\n", cycle, temp, status);
 
      }
+
+     fclose(fichier);
+
+     printf("Données écrites dans le data.txt\n");
 
      return 0;
 }
